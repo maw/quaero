dfded4a5ccc8815f0132ce728bba0f995440b236
Michael Wolf <maw@pobox.com>
2026-02-11 12:02:19 -0500

    Import deadgrep code temporarily

---
+++ tests/test-helper.el
@@ -0,0 +1,18 @@
+;;; test-helper.el --- Helper for tests              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Wilfred Hughes
+
+;; Author:  <me@wilfred.me.uk>
+
+;;; Code:
+
+(require 'ert)
+(require 'f)
+
+(let ((deadgrep-dir (f-parent (f-dirname (f-this-file)))))
+  (add-to-list 'load-path deadgrep-dir))
+
+(require 'undercover)
+(undercover "deadgrep.el" (:exclude "*-test.el"))
+
+;;; test-helper.el ends here
